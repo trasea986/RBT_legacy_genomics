69b237b11b5ed855793223407ad796688a8e79e9
trasea986 <trasea986@hotmail.com>
2021-07-22 12:24:43 -0700

    added NorWeST data to work flow

--- src/01_env_extract.R
+++ src/01_env_extract.R
@@ -10,13 +10,13 @@ legacy_df <- read.csv('./data/legacy_data_formatted.csv')
 #convert to spatial
 legacy_spatial <- vect(legacy_df, geom=c("Longitude", "Latitude"), crs=projection)
 
-#create list of env data
+#create list of env data for ind bioclim files 
 bio_files <- list.files(path = './data/wc2.1_30s_bio', pattern = '*.tif', all.files = TRUE, full.names = TRUE)
 
-#load in the rasters
+#load in the bioclim rasters
 bio_layers <- rast(bio_files)
 
-#going to crop and export to save space prior to dealing with correlated variables
+#going to crop to save space prior to dealing with correlated variables for bioclim
 x_min <- xmin(legacy_spatial)
 x_max <- xmax(legacy_spatial)
 y_min <- ymin(legacy_spatial)
@@ -34,18 +34,59 @@ bio_layers <- crop(bio_layers, ext)
 
 #plot to double check alignment
 plot(bio_layers$wc2.1_30s_bio_1)
+points(legacy_spatial)
+
+
+#... and now NorWeST. multiple basins were combined in ArcGis Pro prior to this step
+nw_lines <- vect('./data/NorWeST_all_basins.shp')
+
+#reproject one bioclim layer for cropping and rasterization template
+bio1 <- bio_layers$wc2.1_30s_bio_1
+bio1 <- project(bio1, "+proj=aea +lat_0=30 +lon_0=-114 +lat_1=43 +lat_2=47 +x_0=1500000 +y_0=0 +datum=NAD83 +units=m +no_defs")
+
+nw_lines_crop <- crop(nw_lines, bio1)
+
 
 #extract values
 #method bilinear used to interpolate based on location within the cell and not just cell value
-bio_values <- extract(bio_layers, legacy_spatial, xy = TRUE, method = 'bilinear')
+bio_values <- terra::extract(bio_layers, legacy_spatial, xy = TRUE, method = 'bilinear')
+
+#for norwest, need list of the columns wanted
+nw_list <- list('ELEV',
+                'CANOPY',
+                'SLOPE',
+                'PRECIP',
+                'CUMDRAINAG',
+                'Flow_Aug',
+                'S1_93_11')
+
+example_rast <- rasterize(nw_lines_crop, bio1, field='ELEV', background = NA, touches = TRUE)
+example_rast <- project(example_rast, "+proj=longlat +datum=WGS84 +no_defs")
+plot(example_rast)
+
+#rasterize the shapefile, reproject, and then extract
+norwest_sites <- lapply(nw_list, function(i){
+  tmp <- rasterize(nw_lines_crop, bio1, field=paste(i), background = NA, touches = TRUE)
+  tmp <- project(tmp, "+proj=longlat +datum=WGS84 +no_defs")
+  terra::extract(tmp, legacy_spatial, xy = TRUE, method = 'bilinear')
+})
+
+#combine all the individual dataframes together
+norwest_df <- do.call("cbind", norwest_sites)
+
+#remove duplicates (id, x, and y repeat many times from cbind, maybe expore left_join in the future here)
+norwest_df <- norwest_df[!duplicated(as.list(norwest_df))]
+
 
 #combine this with any other columns of env data
 #add back in the population and location information
-full_env <- cbind(legacy_df, bio_values)
+full_env <- cbind(legacy_df, bio_values, norwest_df)
 
 #x/y from bio_values and long/lat from legacy_df match (which is good) so going to delete the x and y
+full_env <- full_env[!duplicated(as.list(full_env))]
 full_env$x <- NULL
 full_env$y <- NULL
+full_env$id <- NULL
 
 #export
 write.csv(full_env, file = './outputs/full_env.csv', row.names = FALSE)
--- src/02_corr_test.R
+++ src/02_corr_test.R
@@ -9,15 +9,15 @@ full_env <- read.csv('./outputs/full_env.csv')
 
 #package only wants the matrix of values, so drop first 5 columns (but save stream names, useful for later)
 streams <- full_env$Stream 
-full_env <- full_env[,6:24]
+full_env <- full_env[,6:ncol(full_env)]
 
 #next renaming the columns to just be the biolcim variable # / deal with other shorthand
-colnames(full_env) <- c('bio1', 'bio10', 'bio11', 'bio12', 'bio13', 'bio14', 'bio15', 'bio16', 'bio17', 'bio18', 'bio19', 'bio2', 'bio3', 'bio4', 'bio5', 'bio6', 'bio7', 'bio8', 'bio9')
+colnames(full_env) <- c('bio1', 'bio10', 'bio11', 'bio12', 'bio13', 'bio14', 'bio15', 'bio16', 'bio17', 'bio18', 'bio19', 'bio2', 'bio3', 'bio4', 'bio5', 'bio6', 'bio7', 'bio8', 'bio9', "ELEV", "CANOPY", "SLOPE", "PRECIP", "CUMDRAINAG", "Flow_Aug", "Stream_temp_93_11")
 
 #main correlation plot, then save it
 cor_full <- corr.test(full_env, method = "spearman", adjust = "none")
 cor_full$r[cor_full$p > 0.05] <- 0 #this puts blanks in for not significant correlations
-cor_init_plot <- corrplot(cor_full$r, order = "FPC", method = "number", type = "lower", tl.cex = 0.7, tl.col = rgb(0, 0, 0))
+
 
 png('outputs/init_corr.png',   
     width     = 3.25,
@@ -25,7 +25,7 @@ png('outputs/init_corr.png',
     units     = "in",
     res       = 600,
     pointsize = 4)
-cor_init_plot
+cor_init_plot <- corrplot(cor_full$r, order = "FPC", method = "number", type = "lower", tl.cex = 0.7, tl.col = rgb(0, 0, 0))
 dev.off()
 
 #stepwise removal
@@ -38,7 +38,7 @@ env_cl <- full_env[, -which(colnames(full_env) %in% highlyCorCol)]
 
 #retest correlation and save matrix + plot
 cl_cor <- corr.test(env_cl, method = "spearman")
-cor_final_plot <- corrplot(cl_cor$r, order = "FPC", method = "number", type = "lower", tl.cex = 0.7, tl.col = rgb(0, 0, 0))
+
 
 png('outputs/final_corr.png',   
     width     = 3.25,
@@ -46,7 +46,7 @@ png('outputs/final_corr.png',
     units     = "in",
     res       = 600,
     pointsize = 4)
-cor_final_plot
+corrplot(cl_cor$r, order = "FPC", method = "number", type = "lower", tl.cex = 0.7, tl.col = rgb(0, 0, 0))
 dev.off()
 
 write.csv(env_cl, file = './outputs/env_final_matrix.csv', row.names = FALSE)
