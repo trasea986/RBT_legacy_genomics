16a2a465240e7a693864f5bdc31d8ef4cf3174d5
trasea986 <trasea986@hotmail.com>
2021-07-26 16:46:34 -0700

    removed hatchery location and lower dry creek. added labels to pca plot.

--- src/03_env_PCA.R
+++ src/03_env_PCA.R
@@ -5,6 +5,7 @@ library(FactoMineR)
 library(factoextra)
 #require(MASS)
 library(reshape2)
+library(ggrepel)
 library(cowplot)
 
 
@@ -13,6 +14,9 @@ library(cowplot)
 #first bring in the final output from 02_corr_test.R
 
 env_final <- read.csv('./outputs/env_final_pops_temp_swap.csv')
+
+##Shawn mentioned dropping Aug Flow due to lack of variation
+env_final <- select(env_final, -FLOW_Aug)
 
 #next is bring in the sample request file to add in ecotypes for color coding the PCA
 samples <- read.csv('./data/GEM3 Redband legacy sample request-090419-GIS.csv')
@@ -41,14 +45,17 @@ env_final$Ecotype <- c(
   'Cold Montane', #"South Callahan Creek -> Callahan Creek - HydroID: 6553" 
   'Coastal Hatchery') #"Hayspur Fish Hatchery")
 
-env_final = env_final %>% select(Stream, Ecotype, everything())
+env_final <- env_final %>% select(Stream, Ecotype, everything())
+
+#remove the hatchery and remove lower dry creek
+env_final <- env_final[-c(12, 20),]
 
 write.csv(env_final, file = './outputs/env_final_pca.csv', row.names = FALSE)
 
 #now to do the pca
 #pulling heavily from this website due to not liking my previous PCA scripts: https://tem11010.github.io/Plotting-PCAs/
 
-matrix <- env_final[, 3:11]
+matrix <- env_final[, 3:10]
 
 ## center and scale the data
 # for (i in 1:length(colnames(matrix))){
@@ -60,10 +67,10 @@ matrix <- env_final[, 3:11]
 
 pca1 <- PCA(matrix, graph = FALSE)
 #pull out percentages of variance for first four axes
-PCA1 <- 43.77
-PCA2 <- 26.59
-PCA3 <- 14.25
-PCA4 <- 6.35
+PCA1 <- 42.73
+PCA2 <- 28.00
+PCA3 <- 16.99
+PCA4 <- 5.64
 
 
 #pull in the first few PCAs
@@ -78,19 +85,27 @@ pca.vars$vars <- rownames(pca.vars)
 pca.vars.m <- melt(pca.vars, id.vars = "vars")
 
 matrix$Ecotype <- env_final$Ecotype
+matrix$Stream <- env_final$Stream
+
+matrix <- separate(matrix, Stream, into = c("Stream", "Hydro"), sep = '-')
+
+ 
 
 pc12_points <- ggplot(data = matrix, aes(x = pc1, y = pc2, color = Ecotype, shape = Ecotype)) +
   geom_hline(yintercept = 0, lty = 2) +
   geom_vline(xintercept = 0, lty = 2) +
+  geom_label_repel(aes(label = Stream),
+                   box.padding   = .5, 
+                   #point.padding = 5,
+                   segment.color = 'darkgrey') +
   geom_point(size = 3) +
   stat_ellipse(geom="polygon", aes(fill = Ecotype),  alpha = 0.05, show.legend = FALSE, level = 0.95) +
-  scale_color_manual(values = c('black', 'blue', 'orange', 'red')) +
-  scale_fill_manual(values = c('black', 'blue', 'orange', 'red')) +
-  scale_shape_manual(values=c(8, 15, 19, 17))+
+  scale_color_manual(values = c('blue', 'orange', 'red')) +
+  scale_fill_manual(values = c('blue', 'orange', 'red')) +
+  scale_shape_manual(values=c(15, 19, 17))+
   xlab(paste("PC1 ", PCA1, "%", sep = '')) + 
   ylab(paste("PC2 ", PCA2, "%", sep = '')) +
-  coord_equal() +
-  theme_bw(base_size = 12) +
+  theme_bw(base_size = 13) +
   theme(panel.grid = element_blank(), 
         panel.border = element_rect(fill= "transparent"),
         legend.position = "bottom")
@@ -99,15 +114,18 @@ pc12_points <- ggplot(data = matrix, aes(x = pc1, y = pc2, color = Ecotype, shap
 pc34_points <- ggplot(data = matrix, aes(x = pc3, y = pc4, color = Ecotype, shape = Ecotype)) +
   geom_hline(yintercept = 0, lty = 2) +
   geom_vline(xintercept = 0, lty = 2) +
+  geom_label_repel(aes(label = Stream),
+                   box.padding   = .5, 
+                   #point.padding = 5,
+                   segment.color = 'darkgrey') +
   geom_point(size = 3) +
   stat_ellipse(geom="polygon", aes(fill = Ecotype),  alpha = 0.05, show.legend = FALSE, level = 0.95) +
-  scale_color_manual(values = c('black', 'blue', 'orange', 'red')) +
-  scale_fill_manual(values = c('black', 'blue', 'orange', 'red')) +
-  scale_shape_manual(values=c(8, 15, 19, 17))+
+  scale_color_manual(values = c('blue', 'orange', 'red')) +
+  scale_fill_manual(values = c('blue', 'orange', 'red')) +
+  scale_shape_manual(values=c(15, 19, 17))+
   xlab(paste("PC3 ", PCA3, "%", sep = '')) + 
   ylab(paste("PC4 ", PCA4, "%", sep = '')) +
-  coord_equal() +
-  theme_bw(base_size = 12) +
+  theme_bw(base_size = 13) +
   theme(panel.grid = element_blank(), 
         panel.border = element_rect(fill= "transparent"),
         legend.position = "bottom")
@@ -130,11 +148,11 @@ vars12 <- ggplot() +
   geom_vline(xintercept = 0, lty = 2, color = "grey", alpha = 0.9) +
   geom_segment(data = pca.vars, aes(x = 0, xend = Dim.1, y = 0, yend = Dim.2),arrow = arrow(length = unit(0.025, "npc"), type = "open"),
                lwd = 1) + 
-  geom_text(data = pca.vars, aes(x = Dim.1*1.15, y =  Dim.2*1.15,label = c(colnames(matrix[,1:9]))), check_overlap = F, size = 3) +
+  geom_text(data = pca.vars, aes(x = Dim.1*1.15, y =  Dim.2*1.15,label = c(colnames(matrix[,1:8]))), check_overlap = F, size = 3) +
     xlab(paste("PC1 ", PCA1, "%", sep = '')) + 
     ylab(paste("PC2 ", PCA2, "%", sep = '')) +
     coord_equal() +
-    theme_bw(base_size = 12) +
+    theme_bw(base_size = 13) +
     theme(panel.grid = element_blank(), panel.border = element_rect(fill= "transparent"))
 
 vars34 <- ggplot() +
@@ -143,11 +161,11 @@ vars34 <- ggplot() +
   geom_vline(xintercept = 0, lty = 2, color = "grey", alpha = 0.9) +
   geom_segment(data = pca.vars, aes(x = 0, xend = Dim.3, y = 0, yend = Dim.4),arrow = arrow(length = unit(0.025, "npc"), type = "open"),
                lwd = 1) + 
-  geom_text(data = pca.vars, aes(x = Dim.3*1.15, y =  Dim.4*1.15,label = c(colnames(matrix[,1:9]))), check_overlap = F, size = 3) +
-  xlab(paste("PC1 ", PCA1, "%", sep = '')) + 
-  ylab(paste("PC2 ", PCA2, "%", sep = '')) +
+  geom_text(data = pca.vars, aes(x = Dim.3*1.15, y =  Dim.4*1.15,label = c(colnames(matrix[,1:8]))), check_overlap = F, size = 3) +
+  xlab(paste("PC3 ", PCA3, "%", sep = '')) + 
+  ylab(paste("PC4 ", PCA4, "%", sep = '')) +
   coord_equal() +
-  theme_bw(base_size = 12) +
+  theme_bw(base_size = 13) +
   theme(panel.grid = element_blank(), panel.border = element_rect(fill= "transparent"))
 
 
@@ -155,4 +173,4 @@ plot_grid(pc12_points, vars12, pc34_points, vars34,
           labels = c("A)", "B)", "C)" ,"D)"), nrow = 2)
 
 
-ggsave('./outputs/pca.png', plot = last_plot(), width = 30, height = 20, units = "cm")
+ggsave('./outputs/pca.png', plot = last_plot(), width = 35, height = 35, units = "cm")
