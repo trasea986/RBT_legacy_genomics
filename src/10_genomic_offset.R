1848fe2274dc30b3b0943e79dc45dffd20fcc18b
trasea986 <trasea986@hotmail.com>
2021-11-19 13:15:47 -0800

    updated due to changes with latest LEA version of genomic.offset

--- src/10_genomic_offset.R
+++ src/10_genomic_offset.R
@@ -1,6 +1,6 @@
 #will use predicted WorldClim variables
 #will need to make a new lfmm2 object that is just uses worldclim
-
+#devtools::install_github("bcm-uga/LEA") need most recent version
 library(LEA)
 library(terra)
 
@@ -21,15 +21,17 @@ pop <- env_lfmm[,1]
 env_lfmm <- env_lfmm[,-c(1:6)]
 env_lfmm <- env_lfmm[,-c(6)]
 
-#run lfmm2
-lfmm2_model <- lfmm2(input = rbeta_data, env = env_lfmm, K = 9)
-
-saveRDS(lfmm2_model, '../outputs/lfmm2_model_offset.RDS')
+#older versions of LEA might have needed you to run lfmm2 before, and defined with "object" parameter".
+#run lfmm2, generally needs to be run on a cluster
+#lfmm2_model <- lfmm2(input = rbeta_data, env = env_lfmm, K = 9)
+#saveRDS(lfmm2_model, '../outputs/lfmm2_model_offset.RDS')
+#lfmm2_model <- readRDS('../outputs/lfmm2_model_offset.RDS')
 
 #next, need matrix for the two climate change scenarios. This is going to be very similar to the 01 script with bringing in the legacy data and processing to extract the WorldClim rasters
 #set up points and extract
 #define projection for this project
 projection <- "EPSG: 4326"
+env_final <- read.csv('../outputs/env_final_pca.csv', stringsAsFactors = FALSE)
 legacy_df <- read.csv('../data/legacy_data_formatted.csv')
 
 #remove duplicate rows
@@ -62,6 +64,9 @@ bio_values245$ID <- NULL
 
 colnames(bio_values245) = c(colnames(env_final[,7:11]))
 
+#add row for each individual
+bio_values245 <- bio_values245[rep(seq_len(nrow(bio_values245)), each = 20), ]
+
 #export
 write.csv(bio_values245, file = '../outputs/ssp245_env.csv', row.names = FALSE)
 
@@ -75,22 +80,25 @@ bio_values585$x <- NULL
 bio_values585$y <- NULL
 bio_values585$ID <- NULL
 colnames(bio_values585) = c(colnames(env_final[,7:11]))
+bio_values585 <- bio_values585[rep(seq_len(nrow(bio_values585)), each = 20), ]
+
 write.csv(bio_values585, file = '../outputs/ssp585_env.csv', row.names = FALSE)
-
-
-
 
 
 # Computing genetic offset statistics for 2 populations, defined from latent factor 1
 
-g.offset245 <- genetic.offset(object = lfmm2_model, input = rbeta_data, 
+g_offset245 <- genetic.offset(input = rbeta_data, 
                            env = env_lfmm, new.env = bio_values245, 
-                           pop.labels = pop)
+                           pop.labels = pop, K = 9)
+saveRDS(g_offset245, '../outputs/g_offset245.RDS')
 
-g.offset585 <- genetic.offset(object = lfmm2_model, input = rbeta_data, 
+g_offset585 <- genetic.offset(input = rbeta_data, 
                               env = env_lfmm, new.env = bio_values585, 
-                              pop.labels = pop)
+                              pop.labels = pop, K = 9)
+saveRDS(g_offset585, '../outputs/g_offset585.RDS')
 
+
+#create raster by interpolating the offset values
 #going to crop to save space prior to dealing with correlated variables for bioclim
 x_min <- xmin(legacy_spatial)
 x_max <- xmax(legacy_spatial)
